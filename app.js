17cca34ca2bfe2c50b66de7122cb84fb0ee5cfe1
guren <alix.petit@poles.org>
2022-08-30 10:35:18 +0200

    zombie animation+deplacement

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ console.log('hello');
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 1800,
+    height: 900,
     scene: {
         preload: preload,
         create: create,
@@ -67,30 +67,34 @@ function create ()
         repeat : -1,
     });
 
-    this.anims.create ({
-        key : 'zbn',
-        frames: [
-          {key : "zombie"},  
-        ],
-        frameRate : 10,
-        repeat : -1,
-    });
+    //Animation inutile ci-dessou :
+    // this.anims.create ({
+    //     key : 'zbn',
+    //     frames: [
+    //       {key : "zombie"},  
+    //     ],
+    //     frameRate : 10,
+    //     repeat : -1,
+    // });
 
     var background = this.add.image(400, 300, 'BG');
     player = this.physics.add.sprite(400, 200, 'zombie');
-    player.body.setSize(300,490);
+    player.body.setSize(250,490);
     // player.anims.play("playerwalk");
 
     var platforms = this.physics.add.staticGroup();
-    var sol1 = this.add.sprite(50, 600, 'sol1');
-    var sol2 = this.add.sprite(100, 600, 'sol2');
-    var sol4 = this.add.sprite(150, 600, 'sol2');
-    var sol5 = this.add.sprite(200, 600, 'sol2');
-    var sol6 = this.add.sprite(300, 600, 'sol2');
-    var sol7 = this.add.sprite(400, 600, 'sol2');
-    var sol8 = this.add.sprite(500, 600, 'sol2');
-    var sol9 = this.add.sprite(600, 600, 'sol2');
-    var sol3 = this.add.sprite(700, 600, 'sol3');
+    var sol1 = this.add.sprite(50, 800, 'sol1');
+    var sol2 = this.add.sprite(100, 800, 'sol2');
+    var sol4 = this.add.sprite(150, 800, 'sol2');
+    var sol5 = this.add.sprite(200, 800, 'sol2');
+    var sol6 = this.add.sprite(300, 800, 'sol2');
+    var sol7 = this.add.sprite(400, 800, 'sol2');
+    var sol8 = this.add.sprite(500, 800, 'sol2');
+    var sol9 = this.add.sprite(600, 800, 'sol2');
+    var sol3 = this.add.sprite(700, 800, 'sol3');
+    var sol10 = this.add.sprite(700, 500, 'sol1');
+    var sol11 = this.add.sprite(800, 500, 'sol2');
+    var sol12= this.add.sprite(900, 500, 'sol3');
     platforms.add(sol1);
     platforms.add(sol2);
     platforms.add(sol3);
@@ -100,6 +104,9 @@ function create ()
     platforms.add(sol7);
     platforms.add(sol8);
     platforms.add(sol9);
+    platforms.add(sol10);
+    platforms.add(sol11);
+    platforms.add(sol12);
 
     this.physics.add.collider(platforms,player);
    
@@ -110,25 +117,27 @@ function update(){
     if (cursors.left.isDown)
     {
         player.setVelocityX(-160);
-      
+        player.anims.play("playerwalk",true);
+        player.setFlip(true,false);
     
     }
     else if (cursors.right.isDown)
     {
         player.setVelocityX(160);
         player.anims.play("playerwalk",true);
-   
+        player.setFlip(false,false);
     }
     else
     {
         player.setVelocityX(0);
-        player.anims.play('zbn')
+        // player.anims.play('zbn')
+        player.setTexture('zombie');
         
     }
 
     if (cursors.space.isDown && player.body.touching.down)
     {
-        player.setVelocityY(-530);
+        player.setVelocityY(-555);
     }
 
 
